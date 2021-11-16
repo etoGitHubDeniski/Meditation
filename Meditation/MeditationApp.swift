6185ac1e756a2744c72f635c989bb359943ecd7d
Student <student@402-05.local>
2021-11-16 11:39:52 +0300

    add files

---
+++ Meditation/MeditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationApp.swift
+//  Meditation
+//
+//  Created by Student on 16.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct MeditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
